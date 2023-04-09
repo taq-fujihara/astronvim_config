33fb24ffee1b11638f8f6d6e5d6bf209b29c2a02
taq-fujihara <taq.fujihara@gmail.com>
2023-04-09 11:59:01 +0900

    add keymap

--- mappings.lua
+++ mappings.lua
@@ -6,9 +6,13 @@ return {
     ["gT"] = {
       function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous buffer",
     },
-    ["s"] = {
+    ["f"] = {
       function() require("hop").hint_char2({}) end,
       desc = "HopChar2",
+    },
+    ["<C-\\>"] = {
+      "<cmd>vsplit<cr>",
+      desc = "Vertical Split",
     },
   },
 }
