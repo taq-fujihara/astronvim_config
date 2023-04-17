54794315dc53798e26cbe3e448ea4fe51c8c9fd6
taq-fujihara <taq.fujihara@gmail.com>
2023-04-17 13:16:14 +0900

    Blamer config

--- plugins/blamer.lua
+++ plugins/blamer.lua
@@ -5,5 +5,8 @@ return {
   init = function()
     vim.g.blamer_enabled = 1
     vim.g.blamer_delay = 500
+    vim.g.blamer_prefix = '    '
+    vim.g.blamer_date_format = "%Y/%m/%d"
+    vim.g.blamer_relative_time = 1
   end,
 }
