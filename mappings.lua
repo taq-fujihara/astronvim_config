66854c5443262385203e24069244fd94c8ff8efa
taq-fujihara <taq.fujihara@gmail.com>
2023-07-01 23:15:31 +0900

    install flash.nvim
    not customized much yet.

--- mappings.lua
+++ mappings.lua
@@ -6,10 +6,6 @@ return {
     ["gT"] = {
       function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous buffer",
     },
-    ["f"] = {
-      function() require("hop").hint_char2({}) end,
-      desc = "HopChar2",
-    },
     ["-"] = {
       "<cmd>split<cr>",
       desc = "Horizontal Split",
