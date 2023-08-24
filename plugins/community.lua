9c7d63a224f3203fdd7f6743b1cd7c4928085353
taq-fujihara <taq.fujihara@gmail.com>
2023-08-24 22:40:25 +0900

    terraform

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,12 +1,5 @@
 return {
   "AstroNvim/astrocommunity",
-  -- { import = "astrocommunity.pack.go" },
-  -- { import = "astrocommunity.pack.html-css" },
-  -- { import = "astrocommunity.pack.lua" },
-  -- { import = "astrocommunity.pack.markdown" },
-  -- { import = "astrocommunity.pack.python" },
-  -- { import = "astrocommunity.pack.svelte" },
-  -- { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.dracula-nvim" },
   { import = "astrocommunity.colorscheme.fluormachine-nvim" },
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
   opts = {
     ensure_installed = {
       "scss",
+      "terraform",
     },
   },
 }
