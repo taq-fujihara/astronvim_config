6575ea0a51a932b98425ca327bcf7ded24ec658a
taq-fujihara <taq.fujihara@gmail.com>
2023-08-24 22:51:43 +0900

    markdown

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,6 +5,7 @@ return {
       ensure_installed = {
         -- "cssls",
         -- "denols",
+        "marksman",
         "pyright",
         "eslint",
         -- "rust_analyzer",
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
+      "markdown_inline",
       "scss",
       "terraform",
     },
