728c0396129ced76a763e02ea8a68ca700f2ef39
taq-fujihara <taq.fujihara@gmail.com>
2023-08-27 23:02:43 +0900

    deno

--- init.lua
+++ init.lua
@@ -7,14 +7,16 @@ return {
       },
     },
     config = {
-    --   denols = function(opts)
-    --     opts.root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
-    --     return opts
-    --   end,
-    --   tsserver = function(opts)
-    --     opts.root_dir = require("lspconfig.util").root_pattern "package.json"
-    --     return opts
-    --   end,
+      tsserver = function(opts)
+        opts.root_dir = require("lspconfig.util").root_pattern("package.json")
+        opts.single_file_support = false
+        return opts
+      end,
+      denols = function(opts)
+        opts.root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
+        opts.single_file_support = false
+        return opts
+      end,
     },
   },
 }
