d6680d75b71c6810afe641db9629ed0b9f44b7ed
taq-fujihara <taq.fujihara@gmail.com>
2023-08-27 10:19:42 +0900

    modify vscode settings

--- vscode.lua
+++ vscode.lua
@@ -15,31 +15,76 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.g.mapleader = ' '
 
-require("lazy").setup(
+require("lazy").setup({
   {
     "folke/flash.nvim",
-    version="*",
     event = "VeryLazy",
-    vscode = true,
+    version = "*",
     opts = {},
     keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            search = {
+              mode = function(str)
+                return "\\<" .. str
+              end,
+            },
+            label = {
+              uppercase = false,
+            },
+            autojump = true,
+          })
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Flash Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
     },
   },
   {
     "kylechui/nvim-surround",
-    version = "*",
-    lazy = false,
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup({
-      })
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
     end
-  }
-)
+  },
+})
 
 vim.keymap.set("n", "|", "<Cmd>call VSCodeNotify('workbench.action.splitEditor')<CR>")
 vim.keymap.set("n", "-", "<Cmd>call VSCodeNotify('workbench.action.splitEditorDown')<CR>")
