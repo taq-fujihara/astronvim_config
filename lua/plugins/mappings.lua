0abcb46034561be84ad08f0fb3597aa1bfcca6c8
taq-fujihara <taq.fujihara@gmail.com>
2024-04-19 09:10:39 +0900

    mappings

---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,34 @@
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    mappings = {
+      n = {
+        ["gt"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        ["gT"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        ["-"] = {
+          "<cmd>split<cr>",
+          desc = "Horizontal Split",
+        },
+        ["<C-_>"] = {
+          function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end,
+          desc = "Comment Line",
+        },
+        ["<Leader>j"] = {
+          "*``cgn",
+          desc = "Replace Word under Cursor",
+        },
+        -- ["<Leader>gho"] = {
+        --   "<cmd>GhostTextStart<cr>",
+        --   desc = "Start GhostText",
+        -- },
+      },
+      i = {
+        ["<C-_>"] = {
+          function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end,
+          desc = "Comment Line",
+        },
+      },
+    },
+  },
+}
