33ce9fca9ab8f49aa6b7b696e0fe262dfcdc801b
taq-fujihara <taq.fujihara@gmail.com>
2024-04-19 16:35:36 +0900

    colorscheme

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,8 @@
+return {
+  "rcarriga/nvim-notify",
+  opts = {
+    -- To prevent "Highlight group 'NotifyBackground' has no background highlight"
+    -- error when using catppuccin theme.
+    background_colour = "#000000",
+  },
+}
