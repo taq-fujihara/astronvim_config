e64e00c6e1d55b3a2f2367a7757ab8a99f52cffe
taq-fujihara <taq.fujihara@gmail.com>
2024-04-21 10:12:40 +0900

    oil

--- lua/community.lua
+++ lua/community.lua
@@ -2,5 +2,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.pack.vue" },
 }
