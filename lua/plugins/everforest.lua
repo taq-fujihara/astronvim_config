ce3a521e331e2e625aad889d5d604462d7d09db5
taq-fujihara <taq.fujihara@gmail.com>
2024-04-21 15:53:49 +0900

    modify everforest settings

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -4,12 +4,16 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    vim.o.cursorcolumn = true
     require("everforest").setup {
       background = "hard",
       transparent_background_level = 1,
       on_highlights = function(hl, palette)
         hl.CursorLine = {
-          bg = palette.bg_dim,
+          bg = palette.bg0,
+        }
+        hl.CursorColumn = {
+          bg = palette.bg0,
         }
       end,
     }
