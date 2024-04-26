ae026eada3d49cd734df2f38c9bc6d414e35e018
taq-fujihara <taq.fujihara@gmail.com>
2024-04-26 22:23:37 +0900

    vscode winbar

--- lua/community.lua
+++ lua/community.lua
@@ -3,6 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
   { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
 }
