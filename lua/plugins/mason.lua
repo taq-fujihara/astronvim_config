4dc4a254820b46ebda4cd8954c4c13a34e6a4ade
taq-fujihara <taq.fujihara@gmail.com>
2024-04-29 15:09:51 +0900

    add astro

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,6 +4,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "astro",
         "biome",
         "denols",
         "docker_compose_language_service",
