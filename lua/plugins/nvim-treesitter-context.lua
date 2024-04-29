04f90d90e39625a28b414efed394b1ef459785e6
taq-fujihara <taq.fujihara@gmail.com>
2024-04-29 16:55:40 +0900

    treesitter context and telescope-frecency

---
+++ lua/plugins/nvim-treesitter-context.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  opts = {
+    enable = true,
+    max_lines = 5,
+    min_window_height = 0,
+    line_numbers = true,
+    multiline_threshold = 20,
+    trim_scope = "outer",
+    mode = "cursor",
+    separator = nil,
+    zindex = 20,
+  },
+}
