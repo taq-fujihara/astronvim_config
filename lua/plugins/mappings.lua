9b325c408a548d048acf3a680781d72e91db7639
taq-fujihara <taq.fujihara@gmail.com>
2024-05-21 22:18:21 +0900

    add keybindings to resize pane

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -5,8 +5,15 @@ return {
     mappings = {
       n = {
         [";"] = { ":", desc = "CMD enter command mode" },
+
+        ["<C-S-j>"] = { "<Cmd>resize -2<CR>", desc = "Resize split up" },
+        ["<C-S-k>"] = { "<Cmd>resize +2<CR>", desc = "Resize split down" },
+        ["<C-S-l>"] = { "<Cmd>vertical resize -4<CR>", desc = "Resize split left" },
+        ["<C-S-h>"] = { "<Cmd>vertical resize +4<CR>", desc = "Resize split right" },
+
         L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+
         ["-"] = {
           "<cmd>split<cr>",
           desc = "Horizontal Split",
@@ -15,6 +22,7 @@ return {
           function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end,
           desc = "Comment Line",
         },
+
         ["<Leader>j"] = {
           "*``cgn",
           desc = "Replace Word under Cursor",
