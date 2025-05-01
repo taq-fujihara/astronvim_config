c0323390bddf91227814be2ecaad9999b9f074a5
taq-fujihara <taq.fujihara@gmail.com>
2025-05-01 22:54:03 +0900

    add Copilot Chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,15 @@
 return {
-  "github/copilot.vim",
-  event = "VeryLazy",
+  {
+    "github/copilot.vim",
+    event = "VeryLazy",
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {},
+  },
 }
