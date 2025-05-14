5228ef0420e97c57f73c6b097359d98856a1d927
taq-fujihara <taq.fujihara@gmail.com>
2025-05-14 17:08:37 +0900

    modify copilot configuration

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,58 @@
 return {
   {
-    "github/copilot.vim",
-    event = "VeryLazy",
+    "Saghen/blink.cmp",
+    optional = true,
+    opts = function(_, opts)
+      if not opts.keymap then
+        opts.keymap = {}
+      end
+      opts.keymap["<Tab>"] = {
+        "snippet_forward",
+        function()
+          if vim.g.ai_accept then
+            return vim.g.ai_accept()
+          end
+        end,
+        "fallback",
+      }
+      opts.keymap["<S-Tab>"] = { "snippet_backward", "fallback" }
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = false, -- handled by completion engine
+        },
+      },
+    },
+    specs = {
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          options = {
+            g = {
+              ai_accept = function()
+                if require("copilot.suggestion").is_visible() then
+                  require("copilot.suggestion").accept()
+                  return true
+                end
+              end,
+            },
+          },
+        },
+      },
+    },
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      { "github/copilot.vim" },
+      { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim", branch = "master" },
     },
     build = "make tiktoken",
